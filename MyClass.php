82590b3630ae775e2d83b1143a6c0a3267c3bd1a
Giorgio Sironi <info@giorgiosironi.com>
2013-04-14 18:49:49 +0200

    Completing manually: visibility and method order

--- MyClass.php
+++ MyClass.php
@@ -5,16 +5,16 @@ class MyClass
 {
     private $numbers;
 
+    public function __construct()
+    {
+        $this->numbers = array();
+        $this->numbers[] = '42';
+    }
+
     public function doSomething()
     {
         $this->__construct();
         $result = sprintf('The answer is %s', implode(" or ", $this->numbers));
         return $result;
-    }
-
-    private function __construct()
-    {
-        $this->numbers = array();
-        $this->numbers[] = '42';
     }
 }
